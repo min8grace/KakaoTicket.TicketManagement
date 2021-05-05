b541be315f2928ce8a78e86896e7c2375993d9db
min8grace <56316476+min8grace@users.noreply.github.com>
2021-05-05 14:29:15 -0700

    Adding Unit Tests for the API Layer using Moq, Shouldy, XUnit Libraries

---
+++ KakaoTicket.TicketManagement.Api_IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,38 @@
+using KakaoTicket.TicketManagement.Api;
+using KakaoTicket.TicketManagement.Api_IntegrationTests.Base;
+using KakaoTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KakaoTicket.TicketManagement.Api_IntegrationTests.Controllers
+{
+
+    public class CategoryControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _factory;
+
+        public CategoryControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResult()
+        {
+            var client = _factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("/api/category/all");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<List<CategoryListVm>>(responseString);
+            
+            Assert.IsType<List<CategoryListVm>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
