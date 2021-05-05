3d63e6a6130e4b9bfcbfdcd42c7bd84597bdd888
min8grace <56316476+min8grace@users.noreply.github.com>
2021-05-05 01:40:13 -0700

    Adding Entity Framework Core in the Infrastructure project

---
+++ KakaoTicket.TicketManagement.Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using KakaoTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KakaoTicket.TicketManagement.Persistence.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
---
+++ KakaoTicket.TicketManagement.Persistence/KakaoTicketDbContext.cs
@@ -0,0 +1,39 @@
+﻿using KakaoTicket.TicketManagement.Domain.Common;
+using KakaoTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KakaoTicket.TicketManagement.Persistence
+{
+    public class KakaoTicketDbContext: DbContext
+    {
+        public KakaoTicketDbContext(DbContextOptions<KakaoTicketDbContext> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<AuditableEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
